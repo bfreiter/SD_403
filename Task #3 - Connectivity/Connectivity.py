e92c7828b0daaa74a239c57fcd073c1f974e8a7a
bfuser <bf.user.26@gmail.com>
2025-04-29 13:23:38 -0500

    Finished Conectivity code for Task #3

--- Task #3 - Connectivity/Connectivity.py
+++ Task #3 - Connectivity/Connectivity.py
@@ -1,19 +1,43 @@
 from machine import ADC, Pin
-import time
+import utime
 
-ldr = ADC(26)  # GP26 = ADC0
+#Setup LDR's on A/D pins
+ldr_1 = ADC(26) 
+ldr_2 = ADC(27)
 
+#Setup LEd's
 ledgreen = Pin(0, Pin.OUT)
 ledgreen.off()
+ledblue = Pin(2, Pin.OUT)
+ledblue.off()
 
-# Adjust this based on your ambient light conditions
-threshold = 30000  
+#Define Threshold that voltage (0-65536)
+threshold = 35000  #Voltage threshold that corresponds to LDR value when LED on
+on_time = 1000  #Must be plugged in for 1 second (1000ms) before gate considered connected (avoids misreads)
 
+#Track how long the input has been above threshold
+timer_1 = 0
+timer_2 = 0
+
+#Main Loop
 while True:
-    value = ldr.read_u16()  # Range: 0-65535
+    ad_1 = ldr_1.read_u16() #Reads A/D value for LDR_1
+    ad_2 = ldr_2.read_u16() #Reads A/D value for LDR_2
     
-    if value > threshold:
-        ledgreen.on()
-    else:
+    if ad_1 > threshold: #If the A/D value is higher than threshold, LED should be on
+        timer_1 += 50 #Start adding 50 ms (main loop execution time)
+        if timer_1 >= on_time: #1 second has elapsed
+            ledgreen.on() #Turn on green LED, indicating a module was connected
+    else: #Once A/D value goes below threshold, turn off LED (prevents accidentally thinking gate connected)
+        timer_1 = 0
         ledgreen.off()
         
+    if ad_2 > threshold: #If the A/D value is higher than threshold, LED should be on
+        timer_2 += 50 #Start adding 50 ms (main loop execution time)
+        if timer_2 >= on_time: #1 second has elapsed
+            ledblue.on() #Turn on green LED, indicating a module was connected
+    else: #Once A/D value goes below threshold, turn off LED (prevents accidentally thinking gate connected)
+        timer_2 = 0
+        ledblue.off()
+
+    utime.sleep_ms(50) #Loop every 50ms
